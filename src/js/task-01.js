d9bc443c6d59f0cec6a6b0c70ee8f19380498ea6
lgavlich <lgavlich@gmail.com>
2021-04-22 12:31:03 +0300

    +

---
+++ src/js/task-01.js
@@ -0,0 +1,17 @@
+const navEl = document.querySelector('#categories');
+const ChildrenNav = navEl.children;
+const array = Array.from(ChildrenNav);
+//console.log(array);
+//console.log(ChildrenNav);
+//console.log(navEl.children.length);
+
+function info(element) {
+    const category = element.firstElementChild.textContent;
+    const childrenLength = element.lastElementChild.children.length;
+    //console.dir(element.lastElementChild.children.length);
+return `Категория: ${category}\n Количество элементов: ${childrenLength}\n`
+    
+}
+const abc = array.map(el => info(el))
+
+console.log(...abc);
---
+++ src/js/task-02.js
@@ -0,0 +1,10 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+const item = document.createElement('li');
+console.log(item);
