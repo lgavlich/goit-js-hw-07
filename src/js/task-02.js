61ee23f9e2bc8bf89a1af3e055bd2e514401e5bb
lgavlich <lgavlich@gmail.com>
2021-04-23 19:31:51 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,5 +6,15 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
-const item = document.createElement('li');
-console.log(item);
+const navEl = document.querySelector('#ingredients');
+
+const listItem = ingredients => {
+  return ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    return item;
+  });
+}
+const itemLi = listItem(ingredients);
+navEl.append(...itemLi);
+console.log(itemLi)
