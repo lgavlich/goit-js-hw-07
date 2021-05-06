49eb350a3bff65a808a42a71715abda6d4b17953
lgavlich <lgavlich@gmail.com>
2021-05-06 13:18:07 +0300

    zad 4

---
+++ src/js/task-04.js
@@ -0,0 +1,22 @@
+const targetBtnDec = document.querySelector('button[data-action="decrement"]');
+const targetBtnInc = document.querySelector('button[data-action="increment"]');
+
+const counterValue = document.querySelector('span');
+
+targetBtnInc.addEventListener('click', increment);
+
+targetBtnDec.addEventListener('click', decrement);
+
+//targetBtnInc.addEventListener('click', increment);
+
+function increment(event) {
+    
+    counterValue.textContent = Number(counterValue.textContent) + 1;
+       
+
+}
+function decrement(event) {
+
+    counterValue.textContent = Number(counterValue.textContent) - 1;
+
+ }
